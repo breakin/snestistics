61e7da7370ce6b77c200eb3a7453fd3667f6afab
breakin <breakin@users.noreply.github.com>
2017-11-17 14:56:12 +0100

    Checking in doc to see if I can get hosting to work.

---
+++ docs/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index","userguide/index"],envversion:53,filenames:["index.rst","userguide\\index.rst"],objects:{},objnames:{},objtypes:{},terms:{content:0,guid:0,index:0,page:0,search:0,user:0},titles:["Snestistics documentation","Snestistics User Guide"],titleterms:{document:0,guid:1,indic:0,snestist:[0,1],tabl:0,user:1}})
