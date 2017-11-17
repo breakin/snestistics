a17134db3d62bb3dbda1362e8267e7dea33f9e71
breakin <breakin@users.noreply.github.com>
2017-11-17 15:41:34 +0100

    More work on making documentation work

--- docs_source/make_permanent.bat
+++ docs_source/make_permanent.bat
@@ -5,4 +5,4 @@ del build\.buildinfo
 rmdir /s /q ..\docs
 mkdir ..\docs
 xcopy /E /I build\html ..\docs
-echo Make sure _statis gets moved! > ..\docs\.nojekyll
+echo Make sure _static gets moved! > ..\docs\.nojekyll
