453dd58bbc5429a16912f628eb124eeeb1edb0be
lindqva <anders.lindqvist@autodesk.com>
2017-11-17 15:22:24 +0100

    Doc tests

--- docs_source/make_permanent.bat
+++ docs_source/make_permanent.bat
@@ -1,5 +1,6 @@
 REM Consider moving into cmake
 call make.bat html
+rmdir /s /q build\html\_sources
 rmdir /s /q ..\docs
 mkdir ..\docs
 xcopy /E /I build\html ..\docs
--- docs_source/source/conf.py
+++ docs_source/source/conf.py
@@ -126,17 +126,17 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'simple_nut.png'
+#html_logo = 'simple_nut.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'nut.ico'
+#html_favicon = 'nut.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -168,13 +168,13 @@ html_static_path = ['_static']
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
