b4f865aa75798f8402c16ddaba1e806fa663a62b
breakin <breakin@users.noreply.github.com>
2017-11-17 15:38:38 +0100

    Update doc

--- docs_source/make_permanent.bat
+++ docs_source/make_permanent.bat
@@ -1,6 +1,8 @@
 REM Consider moving into cmake
 call make.bat html
 rmdir /s /q build\html\_sources
+del build\.buildinfo
 rmdir /s /q ..\docs
 mkdir ..\docs
 xcopy /E /I build\html ..\docs
+echo Make sure _statis gets moved! > ..\docs\.nojekyll
