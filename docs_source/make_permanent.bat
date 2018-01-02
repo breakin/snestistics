f9e45e6441b770942c57a9409aae2d86e138db4c
breakin <breakin@users.noreply.github.com>
2018-01-02 22:44:31 +0100

    Removed sphinx documentation

--- docs_source/make_permanent.bat
+++
@@ -1,7 +0,0 @@
-REM Consider moving into cmake
-call make.bat html
-rmdir /s /q build\html\_sources
-del build\.buildinfo
-rmdir /s /q ..\docs\user-guide
-mkdir ..\docs\user-guide
-xcopy /E /I build\html ..\docs\user-guide
--- docs_source/source/command_line_options.rst
+++
@@ -1,7 +0,0 @@
-.. _command_line_options:
-
-
-********************
-Command Line Options
-********************
-Here the command line options for snestistics will be described.
