7ad2c5e02f9495b4e95750c0b9fd873b1a09f6bd
breakin <breakin@users.noreply.github.com>
2018-01-02 22:26:31 +0100

    Trying to make jekyll move the _static-directory that sphinx needs for documentation.

--- docs_source/make_permanent.bat
+++ docs_source/make_permanent.bat
@@ -5,4 +5,3 @@ del build\.buildinfo
 rmdir /s /q ..\docs\user-guide
 mkdir ..\docs\user-guide
 xcopy /E /I build\html ..\docs\user-guide
-echo Make sure _static gets moved! > ..\docs\user-guide\.nojekyll
